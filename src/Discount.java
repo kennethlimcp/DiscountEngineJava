83ee3f60d97074d90533daded8f73bea76c71780
Kenneth Lim <kennethlimcp@gmail.com>
2017-11-03 14:48:03 +0800

    add: more functions!

--- src/Discount.java
+++ src/Discount.java
@@ -1,10 +1,52 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Discount {
+	ArrayList<Integer> productItemList = new ArrayList<Integer>();
+	
 	public static void main(String args[]) {
+		Discount d = new Discount();
+		
 		System.out.println("Hello this is our POS!\n");	
+		Scanner sc = new Scanner(System.in);
+
+		while(true) {
+			System.out.println("\nPlease enter product number: ");	
+			int item = sc.nextInt();
+			
+			d.addItem(item);
+			
+			d.printProductList();
+		}
+		
+//		System.out.println("Going to exit");
+//		sc.close();
 	}
 	
 	public String echo(String s) {
 		return s;
+	}
+
+	public void addItem(int item) {
+		productItemList.add(item);
+	}
+
+	public int getNumberOfItems() {
+		return productItemList.size();
+	}
+
+	public void deleteItem(int item) {
+		int idx = productItemList.indexOf(item);
+		
+		if(idx >= 0) {
+			productItemList.remove(idx);
+		}
+	}
+	
+	public void printProductList() {
+		int productItemSize = getNumberOfItems();
+		for(int i=0; i<productItemSize; i++) {
+			System.out.println(String.valueOf(i) + ". " + String.valueOf(productItemList.get(i)));
+		}
 	}
 }
--- src/DiscountTest.java
+++ src/DiscountTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class DiscountTest extends Discount {
@@ -13,5 +15,45 @@ public class DiscountTest extends Discount {
 		assertEquals("test", result);
 		assertNotEquals("wrong", result);
 	}
+	
+	@Test
+	public void testAddItemToList() {
+		Discount d = new Discount();
+		int numberOfItems = 0;
+		
+		assertEquals(0, numberOfItems);
 
+		d.addItem(1234);
+		
+		numberOfItems = d.getNumberOfItems();
+		
+		assertEquals(1, numberOfItems);
+	}
+	
+	@Test
+	public void testDeleteItemFromList() {
+		Discount d = new Discount();
+		int numberOfItems = 0;
+		
+		d.addItem(1234);
+		numberOfItems = d.getNumberOfItems();
+		
+		assertEquals(1, numberOfItems);
+		
+		d.deleteItem(1234);
+		numberOfItems = d.getNumberOfItems();
+		
+		assertEquals(0, numberOfItems);
+	}
+	
+	@Test
+	public void testDeleteItemWithoutAdd() {
+		Discount d = new Discount();
+		int numberOfItems = 0;
+			
+		d.deleteItem(1234);
+		numberOfItems = d.getNumberOfItems();
+		
+		assertEquals(0, numberOfItems);
+	}
 }
